a2f693c0f6dcc0a82620d8af6e28c7f299b6d5a9
Francisco Aires <francisco.airesjr@gmail.com>
2018-07-14 12:57:42 -0300

    Termina primeira etapa da barra de navegação

---
+++ app/assets/js/horizon-swiper-master/README.markdown
@@ -0,0 +1,33 @@
+# Horizon Swiper
+#### Version 1.1.1
+
+You will love this light (~ 7kb) free swiper.
+It comes with many options and it works with your simple native browser scrolling.
+Try it on touch devices!
+
+## Documentation
+[http://horizon-swiper.sebsauer.de](http://horizon-swiper.sebsauer.de)
+
+### HTML
+```
+<div class="horizon-swiper">
+  <div class="horizon-item"></div>
+  <div class="horizon-item"></div>
+  <div class="horizon-item"></div>
+</div>
+```
+
+### Javascript
+```
+$('.horizon-swiper').horizonSwiper();
+```
+
+### Browser support
+* Google Chrome
+* Mozilla FireFox
+* Safari
+* Internet Explorer (11, 10, 9, 8)
+* Microsoft Edge
+
+## Author
+Sebastian Sauer - [http://www.sebsauer.de](http://www.sebsauer.de)
---
+++ app/views/index.php
@@ -0,0 +1,112 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Cristolândia Masculina em Porto Seguro</title>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    </head>
+    <body>
+        <header class="header">
+            <?php include("includes/header.inc"); ?>
+        </header>
+        <section id="home">
+            <div class="box back">
+                <div class="one">
+                    <h1>Cristolândia Masculina <br> em Porto Seguro - Ba</h1>
+                    <h2>Inauguração: 18/8/18</h2>
+                </div>
+            </div>
+        </section>
+
+        <section id="about">
+            <div class="box">
+                <div class="description">
+                    <h2>Sobre a Cristolândia</h2>
+                    <p>
+                        A Semana de Computação da UFBA está na sua sexta edição! Criada em 2012 pela InfoJr UFBA, a empresa Júnior de Computação da UFBA, a SEMCOMP procura trazer a Salvador o conteúdo necessário para inserir, estimular e desenvolver os profissionais e estudantes na área da Tecnologia da Informação e Comunicação. Esse ano não é diferente. Contamos com diversas palestras, exposições, workshops, competições e muita novidade em conteúdo para você!
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section id="pillars">
+            <div class="box">
+                <div class="box-child" id="inclusion">
+                    <div class="box-i">
+                        <i class="fa fa-hand-peace-o" aria-hidden="true"></i>
+                    </div>
+                    <h3>Inclusão</h3>
+                    <p>A SEMCOMP presa pela promoção da igualdade em todos os âmbitos para inserção de todas as pessoas na computação.</p>
+                </div>
+                <div class="box-child" id="motivation">
+                    <div class="box-i">
+                        <i class="fa fa-hand-rock-o" aria-hidden="true"></i>
+                    </div>
+                    <h3>Motivação</h3>
+                    <p>A SEMCOMP existe para motivar os participantes no desenvolvimento e crescimento do setor de TIC na região.</p>
+                </div>
+                <div class="box-child" id="inovation">
+                    <div class="box-i">
+                        <i class="fa fa-hand-spock-o" aria-hidden="true"></i>
+                    </div>
+                    <h3>Inovação</h3>
+                    <p>A SEMCOMP busca divulgar entre os participantes a importância da busca pela inovação para um maior crescimento da TIC em nossa região.</p>
+                </div>
+            </div>
+        </section>
+
+        <section id="resume">
+            <div class="back">
+                <div class="box">
+                    <div class="little-box" id="date">
+                        <i class="fa fa-calendar" aria-hidden="true"></i>
+                        <h3>4 dias!</h3>
+                    </div>
+                    <div class="little-box" id="congressmen">
+                        <i class="fa fa-users" aria-hidden="true"></i>
+                        <h3>200 Congressistas</h3>
+                    </div>
+                    <div class="little-box" id="ic-speaker">
+                        <i class="fa fa-microphone" aria-hidden="true"></i>
+                        <h3>15 Palestras!</h3>
+                    </div>
+                    <div class="little-box" id="time">
+                        <i class="fa fa-clock-o" aria-hidden="true"></i>
+                        <h3>+60 horas de conteúdo!</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="speakers">
+            <h2>Palestrantes</h2>
+            <div class="box horizon-swiper">
+                <div class="speaker horizon-item">
+                    <div class="speaker-pic">
+                        <img src="../assets/img/alda.jpg">
+                    </div>
+                    <div class="speaker-text">
+                        <h3>Alda Rocha</h3>
+                        <p>
+                            HTML, CSS and UX Instructor - Senior UX Designer and WebDev.
+                            Creator of project @CodamosClub / Amateur Astronomer and Specialist in medieval and Roman Castles.
+                        </p>
+                    </div>
+                </div>
+              
+            </div>
+        </section>
+
+        <section id="schedule">
+
+        </section>
+
+        <footer class="footer">
+            <?php include("includes/footer.inc"); ?>
+        </footer>
+    </body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="../assets/js/javascript.js"></script>
+    <script src="../assets/js/horizon-swiper.js">
+        $('.horizon-swiper').horizonSwiper();
+    </script>
+</html>
