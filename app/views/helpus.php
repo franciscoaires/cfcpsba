0453d442e9181ebf7137a4931e5d830e4d43556b
Francisco Aires <francisco.airesjr@gmail.com>
2018-07-18 12:51:03 -0300

    Pagina de parceria

---
+++ app/views/helpus.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Faça Parte! | Cristolândia Masculina em Porto Seguro</title>
+        <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Oswald:700" rel="stylesheet">
+    </head>
+    <body>
+        <header class="header">
+            <?php include("includes/header.inc"); ?>
+        </header>
+        <section id="helpus-page">
+            <div class="description">
+                <h2>Faça parte!</h2>
+                <div class="img-helpus" id="first">
+                </div>
+                <br>
+                <h3>Interceda!</h3>
+                <br>
+                <p>
+                    Você pode colaborar conosco através da oração. Esse é um forte instrumento do Senhor para
+                    cuidado do Seu povo e é uma importante forma de participar de forma atuante no ministério
+                    de cuidado de gente para a glória do Senhor. Acompanhe-nos no instagram @cristolandiaportoseguro
+                    e saiba nossos pedidos de oração diários. Não esqueça de compartilhar isso conosco! Queremos saber
+                    todos parceiros que temos, especialmente os de oração!
+                </p>
+                <br>
+                <div class="img-helpus" id="second">
+                </div>
+                <br>
+                <h3>Contribua!</h3>
+                <br>
+                <p>
+                    Por meio do PAM Brasil (Parceiros na Ação Missionária) você pode contribuir mensalmente com uma quantia
+                    que auxiliará na manutenção dos projetos Cristolândia em todo Brasil! Contribua consoco e participe
+                    ativamente na obra do Senhor aqui no Brasil!
+                    <br> Clique <a href="https://www.missoesnacionais.org.br/pam-brasil">aqui</a> para saber mais sobre o PAM Brasil!
+                </p>
+                <br>
+                <br>
+                <br>
+                <div class="img-helpus" id="third">
+                </div>
+                <br>
+                <h3>Venha!</h3>
+                <br>
+                <p>
+                    Se você tem interesse em andar conosco na frente missionária da Cristolândia em Porto Seguro, mande-nos
+                    um email ou ligue pra a gente e agende um dia para você e sua caravana virem passar um dia conosco! Precisamos
+                    de apoio e auxílio para caminhar diariamente com 30 homens e criar neles o caráter de Cristo! <br>
+                    Além disso, se você sente um ardor missionário em seu coração, está esperando o que para ser um Radical Cristolândia?
+                    Tendo mais de 18 anos e sendo solteiro, é só se inscrever! Não fique de forma do que Deus está fazendo no Brasil!!
+                    <br> Clique <a href="https://www.radicalbrasil.org/radical-cristolandia">aqui</a> para saber mais sobre o Radical Brasil Cristolândia!
+                </p>
+                <br>
+
+            </div>
+        </section>
+        <footer class="footer">
+            <?php include("includes/footer.inc"); ?>
+        </footer>
+    </body>
+</html>
